21d29089d09d7009103eff9b78e39f16dab017ee
Iftekharul01 <iftekharulislam001@gmail.com>
2025-02-19 13:00:06 +0600

    design01

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -13,29 +13,65 @@ class Lab05 extends StatelessWidget {
     return MaterialApp(
       title: "Star Icon",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Star Icon"),
-            backgroundColor: Colors.amber,
-          ),
-          body: Container(
-            width: double.infinity,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate(
-                100, 
-                (index)=>Icon(
-                  Icons.airplanemode_off,
-                  color: Colors.blue,
-                  size: index.toDouble(),
-                ),
+        appBar: AppBar(
+          title: Text("Star Icon"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: Container(
+          height: 500,
+          margin: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2),
+              borderRadius: BorderRadius.circular(20)),
+          padding: EdgeInsets.all(100),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Text(
+                      "50% OFF",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    Icons.add_circle_outline,
+                    color: Colors.red,
+                  ),
+                ],
               ),
-            ),
-            ),
+              Image(
+                image: AssetImage("assets/shoe.png"),
+                height: 200,
+                width: 350,
+              ),
+              Text("Nike Air Shoes"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "\$450",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "\$500",
+                    style: TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          ),
+        ),
+      ),
     );
   }
 }
